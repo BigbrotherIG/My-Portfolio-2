f5d9dcc60cd0db7cbba915fdec5007e5e2d3c184
itzbigbroda@gmail.com <godwinunini@gmail.com>
2022-12-15 18:31:19 +0100

    portfolio2 Update

---
+++ validate.php
@@ -0,0 +1,11 @@
+<?php
+    if(isset($_POST['submitform'])){
+
+        $name = $_POST['fullname'];
+        $email = $_POST['email'];
+        $message = $_POST['messsage'];
+
+     echo $name ;
+
+    }
+?>
